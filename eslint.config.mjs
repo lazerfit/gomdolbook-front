22058bdb3b0222ede57693c47e3d2fad50cbfb9e
sg kim <redkafe98@gmail.com>
2024-12-16 18:11:27 +0900

    refactor: eslint config 설정 추가
    - eslint-plugin-react, eslint-plugin-react-hooks,
      eslint-plugin-react-refresh, eslint-plugin-jsx-a11y,
      eslint-plugin-prettier 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,50 @@
-import tseslint from 'typescript-eslint'
-import eslint from '@eslint/js'
-import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import { fixupConfigRules } from '@eslint/compat';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
-  {
-    ignores: ['**/build/**', '**/dist/**', '**/*.mjs']
-  },
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  prettierConfig,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname
-      }
-    }
-  }
-)
+	{
+		ignores: ['**/build/**', '**/dist/**', '**/*.mjs'],
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	jsxA11y.flatConfigs.recommended,
+	prettierPluginRecommended,
+	{
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: { jsx: true },
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+			globals: { ...globals.browser },
+		},
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,jsx}'],
+		extends: [tseslint.configs.disableTypeChecked],
+	},
+	{
+		plugins: {
+			react: reactPlugin,
+			'react-hooks': fixupConfigRules(reactHooksPlugin),
+			'react-refresh': reactRefresh,
+		},
+		settings: { react: { version: 'detect' } },
+		rules: {
+			...reactPlugin.configs['recommended'].rules,
+			...reactPlugin.configs['jsx-runtime'].rules,
+			...reactHooksPlugin.configs.recommended.rules,
+
+			'react-refresh/only-export-components': 'warn',
+			'@typescript-eslint/no-unused-vars': 'warn',
+		},
+	}
+);

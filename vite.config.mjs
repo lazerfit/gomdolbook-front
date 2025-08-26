74a6e435e1279fbead0dd3be727219d8229f1ddc
sg kim <redkafe98@gmail.com>
2025-08-26 16:42:12 +0900

    add vite-tsconfig-paths

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,28 +2,23 @@ import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: '127.0.0.1',
     port: 3000,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
     extensions: ['.ts', '.tsx', 'js', '.jsx'],
   },
   test: {
     globals: true,
     environment: 'jsdom',
     include: ['**/?(*.)test.ts?(x)'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
